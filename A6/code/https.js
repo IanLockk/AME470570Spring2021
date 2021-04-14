fb7c4fac34dfef75b5f61f370a61e925f62d09dc
IanLockk <ilockwood00@gmail.com>
2021-04-14 15:28:54 -0700

    done

--- A6/code/https.js
+++ A6/code/https.js
@@ -2,10 +2,11 @@ var homeDir = require('path').join(require('os').homedir());
 require('greenlock-express').create({
   version: 'draft-11'
 , server: 'https://acme-v02.api.letsencrypt.org/directory'
-, email: 'ilockwoo@asu.edu'                                     // CHANGE THIS
+, email: ''                                     // CHANGE THIS
 , agreeTos: true
 , approveDomains: [ 'example.com', 'www.example.com' ]              // CHANGE THIS
 , store: require('greenlock-store-fs')
 , configDir: homeDir
 , app: require('./server.js')
 }).listen(8080, 8443);
+
